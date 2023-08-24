3c286ee1ae61e7b19c6f310da2c0834cda8851fc
doxxx <doxxx93@gmail.com>
2023-08-24 18:19:27 +0900

    green: dollar side effect test success

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -6,7 +6,7 @@ public class Dollar {
     }
 
 
-    public void times(int multiplier) {
-        amount *= multiplier;
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
     }
 }
--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,13 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DollarTest {
 
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
         assertThat(product.amount).isEqualTo(10);
-        five.times(3);
+        product = five.times(3);
         assertThat(product.amount).isEqualTo(15);
     }
 }
