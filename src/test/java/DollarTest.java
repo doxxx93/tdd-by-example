7eaf4e913ff48dacd33f0131f948ab12723edc84
doxxx <doxxx93@gmail.com>
2023-08-25 20:52:18 +0900

    red: currency conversion in Bank test

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -57,4 +57,12 @@ public class DollarTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertThat(result).isEqualTo(Money.dollar(1));
     }
+
+    @Test
+    void testReduceMoneyDifferentCurrency(){
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertThat(result).isEqualTo(Money.dollar(1));
+    }
 }
