3191156d5fe5f6870d417370421ed867f18cc3c2
doxxx <doxxx93@gmail.com>
2023-08-25 21:00:21 +0900

    red: Refactor currency conversion logic from Money to Bank
    - 아직도 2가 테스트와 코드 두 부분에 모두 나온다.
    - 이를 없애기 위해 Bank에서 환율표를 가지고 있다가, 필요할 때 찾아볼 수 있게 해야한다.
      - 두 개의 통화와 환율을 매핑시키는 해시 테이블을 사용할 수 있겠다.
      - 통화 쌍을 해시 테이블의 키로 쓰기 위해 배열을 사용할 수 있을까?
      - Arrays.equals()는 각각의 원소에 대해 동치성 검사를 수행하지 못하므로, 키를 위한 객체를 따로 만든다.

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -3,12 +3,12 @@ public class Sum implements Expression {
     Money addend;
 
     public Sum(Money augend, Money addend) {
-        this.augend= augend;
-        this.addend= addend;
+        this.augend = augend;
+        this.addend = addend;
     }
 
-    public Money reduce(String to) {
-        int amount= augend.amount + addend.amount;
+    public Money reduce(Bank bank, String to) {
+        int amount = augend.amount + addend.amount;
         return new Money(amount, to);
     }
 }
