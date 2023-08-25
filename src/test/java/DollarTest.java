a16d3e160853000fdc56983db98aa418ff1e48cf
doxxx <doxxx93@gmail.com>
2023-08-25 21:13:01 +0900

    green: added mixed addition test success
    - 사실 Expression이어야 하는 Money들을 조금씩 쪼아 없앤다.

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -72,8 +72,8 @@ public class DollarTest {
 
     @Test
     void testMixedAddition() {
-        Expression fiveBucks = Money.dollar(5);
-        Expression tenFrances = Money.franc(10);
+        Money fiveBucks = Money.dollar(5);
+        Money tenFrances = Money.franc(10);
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrances), "USD");
