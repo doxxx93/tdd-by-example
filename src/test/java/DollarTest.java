3178df9fb5433d022cd01169e9b1e7685a3bbe4d
doxxx <doxxx93@gmail.com>
2023-08-25 20:31:34 +0900

    red: simple addition test

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -26,7 +26,10 @@ public class DollarTest {
 
     @Test
     void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertThat(sum).isEqualTo(Money.dollar(10));
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertThat(reduced).isEqualTo(Money.dollar(10));
     }
 }
